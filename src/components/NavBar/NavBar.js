05fff32a8420c3b54b3dc171f5d436f769256951
Kendall <ktrudickm@gmail.com>
2022-01-11 18:59:02 -0500

    NavBar edits

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import { 
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from
 
 const NavBar = () => {
     return (
         <>
-            <div className='Nav'>
-                <div className='NavbarContainer'>
-                    
-                </div>
-            </div>
-            
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='home' smooth={true} offset={-40} duration={1000}>Avocado</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about' smooth={true} offset={-40} duration={1000}>About us</NavLinks>
+                        </NavItem>
+                        <NavBtn>
+                            <NavBtnLink href='http://eepurl.com/hHIAf9' target="_blank">Download the App</NavBtnLink>
+                        </NavBtn>
+                    </NavMenu>
+                </NavbarContainer>
+            </Nav>
         </>
     )
 }
